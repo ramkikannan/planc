21fa405efdbe36c48d7b785c935d5e31103b69dc
Ramakrishnan Kannan <ramakicha@yahoo.com>
2018-09-24 09:41:54 -0400

    checking out ntfdev. stashing existing contents

--- latex/toms/data/timebreakdown.py
+++ latex/toms/data/timebreakdown.py
@@ -19,9 +19,9 @@ df2=df2.set_index('alg-p')
 f = plt.figure()
 plt.title(title, color='black')
 df2.plot(kind='bar', stacked=True, ax=f.gca())
-plt.legend(loc='center right', ncol=3)
+plt.legend(loc=9, ncol=3)
 plt.ylabel('Running Time (in Secs)')
-plt.gcf().set_size_inches(11, 8)
+plt.gcf().set_size_inches(9, 8)
 plt.savefig(output_file,format='pdf')
 
 print 'check output file ' + output_file
