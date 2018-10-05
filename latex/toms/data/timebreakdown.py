cd0207b42ee53deb5fb9a405ad8fabcd560569ff
Ramakrishnan Kannan <ramakicha@yahoo.com>
2018-10-05 09:34:30 -0400

    could not spend more time

--- latex/toms/data/timebreakdown.py
+++ latex/toms/data/timebreakdown.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+from matplotlib import colors as mcolors
 
 if len(sys.argv) < 4:
     print 'Usage: python timebreakdown.py input_file output_file title'
@@ -18,7 +19,21 @@ df2=df2.set_index('alg-p')
 
 f = plt.figure()
 plt.title(title, color='black')
-df2.plot(kind='bar', stacked=True, ax=f.gca())
+#my_colors = 'rgbymcw'
+#ax = df2.plot(kind='bar', stacked=True, ax=f.gca(), color=my_colors)
+ax = df2.plot(kind='bar', stacked=True, ax=f.gca())
+bars = ax.patches
+patterns =(None, None, None, None, '-', '+', 'x') #,'/','//','O','o','\\','\\\\')
+#patterns_color = (None, None, None, None, 'r', 'g', 'b')
+#color_types=('r', 'g', 'b', 'm', None, None, None)
+hatches = [p for p in patterns for i in range(len(df2))]
+#hatch_colors = [p for p in patterns_color for i in range(len(df2))]
+#fill_colors = [p for p in color_types for i in range(len(df2))]
+#for bar, hatch, hatch_color, fill_color in zip(bars, hatches): # hatch_colors, fill_colors):
+for bar, hatch in zip(bars, hatches): # hatch_colors, fill_colors):
+    bar.set_hatch(hatch)
+    #bar.set_color(fill_color)
+    #bar.set_hatch_color(mcolors.rgb_to_hsv(hatch_color))
 plt.legend(loc=9, ncol=3)
 plt.ylabel('Running Time (in Secs)')
 plt.gcf().set_size_inches(9, 8)
